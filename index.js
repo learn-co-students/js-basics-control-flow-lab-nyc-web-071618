31390a6532733e057f8936341c00075d322a8d5b
github username <github email address>
2018-08-26 21:06:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  let response;
+  if (someNumber <= 400) {
+    response = 'This one is on me!';
+  } else if (someNumber > 2000 && someNumber <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (someNumber > 2500) {
+    response = 'No can do.';
+  }
+  return response;
+}
+
+
+function ternaryCheckCity(city) {
+  let response;
+  city === 'NYC' ? response = "Ok, sounds good." : response = "No go.";
+  return response;
+}
+
+function switchOnCharmFromTip(tip){
+  let response;
+  if (tip === 'generous') {
+    return "Thank you so much.";
+  } else if (tip === 'not as generous') {
+    return "Thank you.";
+  } else  {
+    return "Bye.";
+  }
+  return response;
+}
